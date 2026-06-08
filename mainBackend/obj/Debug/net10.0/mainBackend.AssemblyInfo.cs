0555c1bebdf3657b827c6417c93c920bade768d8
Denx26 <denisiosif26@gmail.com>
2026-06-08 13:46:10 +0300

    Auto changes

--- mainBackend/obj/Debug/net10.0/mainBackend.AssemblyInfo.cs
+++ mainBackend/obj/Debug/net10.0/mainBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mainBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3461e4f8ae45304c3a56d4d01ac16dda22fa6257")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4956078cbd74f616d48343b30323ef3c4b61a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("mainBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mainBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
