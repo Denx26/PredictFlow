bcf278a2f0b2f9db51e601411a0319f794cc88d3
Denx26 <denisiosif26@gmail.com>
2026-06-08 17:47:14 +0300

    small verify

--- mainBackend/Controllers/AuthController.cs
+++ mainBackend/Controllers/AuthController.cs
@@ -70,14 +70,14 @@ namespace mainbackend.Controllers
 
     public class RegisterDto
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
     }
     public class LoginDto
     {
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
-        public string Password { get; set; }
+        public string? Password { get; set; }
     }
 }
